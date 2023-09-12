71084673e0d540f929fc571042b006b86c81b1f0
Dipesh Shome <C:/Users/DIPESH SHOME>
2023-09-12 22:30:52 +0600

    data ingestion done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.TimeSeriesProject import logger
+from src.TimeSeriesProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to out custom log')
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
